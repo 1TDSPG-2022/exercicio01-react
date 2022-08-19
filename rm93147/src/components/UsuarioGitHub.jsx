33f1b6124ec8201ffb2d62f0dc9f72f8fc3d25fc
VitorioRossi <rm93147@fiap.com.br>
2022-08-19 09:52:36 -0300

    Feito

---
+++ rm93147/src/components/UsuarioGitHub.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+
+
+function UsuariosGithub() {
+
+    const [relogio, setRelogio] = useState(new Date().toLocaleTimeString())
+
+   
+
+setTimeout(() => {
+
+    setRelogio(new Date().toLocaleTimeString())
+
+}, 1000);
+
+
+
+    return (
+
+      <div>
+
+        <h1>Eu sou o usuario</h1>
+
+        <h3>Horário : {relogio}</h3>
+
+      </div>
+
+    );
+
+  }
+
+ 
+
+  export default UsuariosGithub;
