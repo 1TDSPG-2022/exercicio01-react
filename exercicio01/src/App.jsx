2a1b6a4b0a706b008c71f1c3433b2240de4309ea
PedroLu1z <rm95189@fiap.com.br>
2022-08-19 09:05:07 -0300

    feito 2, 3, 4

---
+++ exercicio01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RM95189 from './components/RM95189'
+
+export default App;
+
+function App(){
+    return(
+        <div>
+            <RM95189/>
+        </div>
+    )
+}
