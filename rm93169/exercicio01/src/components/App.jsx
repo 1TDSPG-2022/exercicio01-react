7e706f7cb34d803b3a8a2649aa05786f386a2584
RossiMurilo <rm93169@fiap.com.br>
2022-08-19 09:48:26 -0300

    Criando APP REACT

---
+++ rm93169/exercicio01/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UsuariosGithub from './UsuariosGithub/UsuariosGithub'
+export default function App() {
+  return (
+    <div>
+      <h1>EXERCÍCIO DE STATE E USE-EFFECTS</h1>
+      <UsuariosGithub/>
+    </div>
+  )
+}
