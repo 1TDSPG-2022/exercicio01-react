977eaefcc385a4631799b1fa893b7c81305780a4
lelemaia <rm94222@fiap.com.br>
2022-08-19 09:39:55 -0300

    arrumar

---
+++ exercicio01-react/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UsuariosGithub from './usuariosGithub/UsuariosGithub'
+
+export default function App (){
+    return(
+        <div>
+            <h1>EXERCICIOS DE STATES E USE-EFFECTS</h1>
+            <UsuariosGithub/>
+        </div>    
+    )
+}
---
+++ exercicio01-react/src/components/usuariosGithub/UsuariosGithub.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+export default function UsuariosGithub() {
+
+    const [relogio, setRelogio] = useState(new Date().toLocaleTimeString)
+
+    setTimeout(() => {
+        setRelogio(new Date().toLocaleTimeString())
+    }, 1000);
+
+    return(
+        <div>
+            <h2>Usuarios do Github</h2>
+            <h3>Horário: {relogio}</h3>
+        </div>
+    )
+}
