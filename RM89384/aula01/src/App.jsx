7e66b9b22ca7de8c929816632ea403534f6390bb
GuilhermeMenezesdaSilva <rm89384@fiap.com.br>
2022-08-19 09:06:08 -0300

    Importado componente UsuariosGithub

--- RM89384/aula01/src/App.jsx
+++ RM89384/aula01/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import UsuariosGithub from './components/UsuariosGithub';
+
 function App() {
   return (
     <div>
         <h1>OLÁ, MUNDO!</h1>
+        <UsuariosGithub/>
     </div>
   )
 }
