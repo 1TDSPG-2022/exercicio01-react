2eacc7be90164f67af9cdd2403ce3027bd759a55
letyciacast <101400712+letyciacast@users.noreply.github.com>
2022-08-19 09:20:46 -0300

    commit1

---
+++ exercicio1/src/componentes/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UsuariosGitHub from "./UsuariosGithub";
+
+export default function App(){
+    return(
+        <div>
+            <h1>ola</h1>
+            <UsuariosGitHub/>
+        </div>
+    )
+}
