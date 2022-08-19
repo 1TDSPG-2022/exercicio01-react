b5f5fe425803f9055d99aa6b84ad2d5d13705035
brunoecp <rm95315@fiap.com.br>
2022-08-19 09:40:04 -0300

    relogio

--- exercicio1/src/App.jsx
+++ exercicio1/src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import UsuariosGitHub from "./componente/UsuarioGitGub";
+import React, {useState, useEffect}from "react";
+import UsuariosGitHub from "./componente/UsuariosGitHub/UsuarioGitGub";
 
 export default function App(){
+
+    const [relogio, setRelogio] = useState(new Date().toLocaleDateString())
+
+    setTimeout(() => {
+        setRelogio(new Date().toLocaleTimeString())
+    }, 1000);
+
+    // useEffect(() => {
+    //   first
+    
+    //   return () => {
+    //   second
+    //   }
+    // }, [third])
+    
+
 return(
-    <div>
+    <div>  
+        <h1>Exercicio de State e Use-Effects</h1>
         <UsuariosGitHub/>
-        <h1>Bom dia</h1>
+        <p>Horario : {relogio}</p>
     </div>
 )}
