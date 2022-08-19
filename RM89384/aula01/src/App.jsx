5758bc2c4522ec864d4bbe1c7d69cc0779fe4e15
GuilhermeMenezesdaSilva <rm89384@fiap.com.br>
2022-08-19 09:40:18 -0300

    Atualizada a formatação

--- RM89384/aula01/src/App.jsx
+++ RM89384/aula01/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import UsuariosGithub from './components/UsuariosGithub';
 
-function App() {
+export default function App() {
   return (
     <div>
-        <h1>OLÁ, MUNDO!</h1>
+        <h1>Exercício de state e use-effects</h1>
         <UsuariosGithub/>
     </div>
   )
 }
-
-export default App;
