b8351471c43b8697ffdd3f314e01d2623ada0747
letyciacast <101400712+letyciacast@users.noreply.github.com>
2022-08-19 09:44:00 -0300

    commit2

--- exercicio1/src/componentes/App.jsx
+++ exercicio1/src/componentes/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import UsuariosGitHub from "./UsuariosGithub";
 
 export default function App(){
+    const [relogio, setRelogio] = useState(new Date().toLocaleTimeString())
+
+    setTimeout(() => {
+        setRelogio(new Date().toLocaleTimeString())
+    }, 1000);
     return(
         <div>
             <h1>ola</h1>
             <UsuariosGitHub/>
+            <h3>Horario: {relogio}</h3>
         </div>
     )
 }
