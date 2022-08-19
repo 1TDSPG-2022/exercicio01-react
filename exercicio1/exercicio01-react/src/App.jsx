27ba5b74ad2bd8b7ea0f93e0eee28ba1f122db93
FlipOwz <rm95122@fiap.com.br>
2022-08-19 09:39:42 -0300

    horario adicionado

--- exercicio1/exercicio01-react/src/App.jsx
+++ exercicio1/exercicio01-react/src/App.jsx
@@ -1,3 +1,13 @@
 import React from 'react'
-import UsuariosGithub from './components/UsuariosGithub'
+import UsuariosGithub from './components/UsuariosGithub/UsuariosGithub'
+
+export default function App (){
+    return(
+        <div>
+            <h1>EXERCÍCIOS DE ATATE E UESE-EFFECTS</h1>
+            <UsuariosGithub/>
+            <relogio/>
+        </div>
+    )
+}
 
---
+++ exercicio1/exercicio01-react/src/components/UsuariosGithub/UsuariosGithub.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+export default function UsuariosGithub(){
+    
+    const [relogio, setRelogio] = useState(new Date().toLocaleTimeString())
+
+    setTimeout(() => {
+         setRelogio(new Date().toLocaleTimeString())
+    }, 1000);
+    
+    return(
+    <div>
+        <h2>Usuário do GitHub</h2>
+        <h3>HORÁRIO: {relogio}</h3>
+    </div>
+    )
+}
