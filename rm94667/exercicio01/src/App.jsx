d331e9faa4a2239db778d0bf7be2eb2e219ac020
Felipe <rm94667@fiap.com.br>
2022-08-19 09:12:09 -0300

    Componentes Importados.

--- rm94667/exercicio01/src/App.jsx
+++ rm94667/exercicio01/src/App.jsx
@@ -1,7 +1,10 @@
+import UsuariosGithub from "./UsuariosGithub";
+
 function App() {
   return (
     <div>
       <h1>Hello Word.</h1>
+      <UsuariosGithub/>
     </div>
   );
 }
---
+++ rm94667/exercicio01/src/UsuariosGithub.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UsuariosGithub() {
+    return (
+      <div>
+        <h1>Hello Word.</h1>
+      </div>
+    );
+  }
+  
+  export default UsuariosGithub;
+  
