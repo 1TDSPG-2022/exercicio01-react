6d062f660bc34fba17edd17f0fded102a4db835e
JoaoSorriso <rm92119@fiap.com.br>
2022-08-19 09:45:27 -0300

    Parte Difícil

---
+++ exercicio01/rm92119/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UsuariosGithub from './usuariosGitHub/UsuariosGithub'
+
+export default function App() {
+  return (
+    <div>
+      <h1>EXERCÍCIO DE STATE E USE-EFFECTS</h1>
+      <UsuariosGithub/>
+    </div>
+  )
+}
