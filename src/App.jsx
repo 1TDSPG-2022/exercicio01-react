871d1d92e700364cecf7cc87ffeaf24d32ae891e
ghorita <rm94860@fiap.com.br>
2022-08-19 09:40:06 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ export default function App(){
+    return(
+        <div>
+            <h1>EXERCÍCIO DE STATE E USE-EFECTS</h1>
+        </div>
+    )
+}
---
+++ src/components/usuariosGitHub/UsuariosGithub.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+export default function UsuariosGithub() {
+
+      const [relogio, setRelogio] = useState(new Date().toLocaleDateString()) 
+
+      setTimeout(() => {
+        setRelogio(new Date().toLocaleTimeString())
+      }, 1000);
+
+       
+    return(
+        <div>
+            <h2>Usuário do Github</h2>
+            <h3>HORÁRIO : {relogio}</h3>
+        </div>
+    )
+}
