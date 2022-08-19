28d48ce6bea6998b41af510d3c1a1febf8fd811a
vSeiji <rm93380@fiap.com.br>
2022-08-19 09:40:56 -0300

    Mudanças no App, Usuario_Github e javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
 import React from 'react'
+import Usuario_Github from './components/Usuario_Github'
+
+export default function App(){
+    return (
+    <div>
+        <h1>Exercicio de State e Use-Effects</h1>
+        <Usuario_Github></Usuario_Github>
+    </div>
+    )
+}
--- src/components/Usuario_Github.jsx
+++ src/components/Usuario_Github.jsx
@@ -1,1 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+export default function Usuario_Github(){
+    
+const [relogio, setRelogio] = useState (new Date().toLocaleDateString())
+
+setTimeout(() => {
+    setRelogio(new Date().toLocaleTimeString())
+}, 1000);
+
+
+    return(
+        <div>
+            <h2>Usuario do Github</h2>
+            <h3>Agora são: {relogio}</h3>
+        </div>
+    )
+}
